72cc464ced2cc4fe0c150c1878b7222a3bfc95e9
Felipe <fellipefreiire3@gmail.com>
2021-10-19 20:54:29 -0300

    solid SRP

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { Category } from '../model/Category';
 import { CategoryRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoryRepository();
@@ -8,13 +8,9 @@ const categoriesRepository = new CategoryRepository();
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
 
-  if (categoryAlreadyExists) {
-    return res.status(400).json({ error: 'Category already exists' })
-  }
-
-  categoriesRepository.create({ name, description })
+  createCategoryService.execute({ name, description })
 
   return res.status(201).send();
 })
