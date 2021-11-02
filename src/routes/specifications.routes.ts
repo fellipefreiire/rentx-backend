cd690642b4ecda85e0d252e1f632955f7e4a3d94
Felipe <fellipefreiire3@gmail.com>
2021-11-01 22:44:56 -0300

    ensure aunthenticated

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
+import { ensureAunthenticated } from '../middlewares/ensureAunthenticated'
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController'
 
 const specificationsRoutes = Router()
 
 const createSpecificationController = new CreateSpecificationController()
 
+specificationsRoutes.use(ensureAunthenticated)
 specificationsRoutes.post('/', createSpecificationController.handle)
 
 export { specificationsRoutes }
