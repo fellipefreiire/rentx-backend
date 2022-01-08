57c0fc4ad87b0d869fd2200a20eb25c53d8fcfdf
Felipe <fellipefreiire3@gmail.com>
2022-01-08 18:37:35 -0300

    rentals migration

--- src/shared/infra/typeorm/migrations/1635450644149-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1635450644149-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1635450644149 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'uuid'
+                        type: 'uuid',
+                        isPrimary: true,
                     },
                     {
                         name: 'name',
