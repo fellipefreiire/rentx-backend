d46284f9113a58bb1bf7a1a6ebc85f13db4e6245
Felipe <fellipefreiire3@gmail.com>
2022-01-10 21:56:28 -0300

    supertest config

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,8 @@
+import request from 'supertest'
+import { app } from '@shared/infra/http/app'
+
+describe('Create Category Controller', () => {
+  it('', async () => {
+    await request(app)
+  })
+})
