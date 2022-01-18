28d69a1fcb4d2cdb032e21d97b39f61f6cc0348e
Felipe <fellipefreiire3@gmail.com>
2022-01-17 23:09:39 -0300

    avatar url config

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -11,12 +11,15 @@ import '@shared/container';
 import { router } from './routes';
 import swaggerFile from '../../../swagger.json';
 import { AppError } from '@shared/errors/AppError';
+import upload from '@config/upload';
 
 createConnection();
 const app = express();
 
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 app.use(router)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
